9214a48f894f3b03c76a537686356f424dab36a7
Dragagon <dragagon.draga@hotmail.com>
2024-05-03 06:21:14 +0200

    finished first iteration of the addon. Added features are automatically adding comment to newly joined members, assigning their correct guild rank and handing out tabard permission automatically. It also adds their names to the daily guild message as a reminder for the welcome mails. As is policy within our guild. Small addition is also getting to the mail interface faster as it skips the dialogue of the mail NPC.

--- SourceCode.lua
+++ SourceCode.lua
@@ -1,16 +1,55 @@
---Use common.LogInfo("common", "-"..name.."-") --Log to mods.txt
---Use tostring() to concatenate non-string values in chat()
---itemLib.GetName(itemId) gets the name of the itemId. Wrap userMods.FromWString() around it for proper output.
-
 function Main()
-	
-	--common.RegisterEventHandler(EVENT_LOOT_TO_DISTRIBUTE, "EVENT_LOOT_TO_DISTRIBUTE")
+	common.RegisterEventHandler(EVENT_GUILD_DECLINE_BUSY, "EVENT_GUILD_DECLINE_BUSY") --https://alloder.pro/md/LuaApi/EventGuildDeclineBusy.html
+	common.RegisterEventHandler(EVENT_GUILD_DECLINE_IGNORED, "EVENT_GUILD_DECLINE_IGNORED") --https://alloder.pro/md/LuaApi/EventGuildDeclineIgnored.html
+	common.RegisterEventHandler(EVENT_GUILD_MEMBER_ADDED, "EVENT_GUILD_MEMBER_ADDED") --https://alloder.pro/md/LuaApi/EventGuildMemberAdded.html
+	common.RegisterEventHandler(EVENT_TALK_STARTED, "EVENT_TALK_STARTED") --https://alloder.pro/md/LuaApi/EventTalkStarted.html
+end
+
+function EVENT_TALK_STARTED()
+	if mailBox.IsInteracting() then --https://alloder.pro/md/LuaApi/FunctionMailBoxIsInteracting.html
+		mailBox.Open()
+	end
+end
+function EVENT_GUILD_DECLINE_BUSY(params) --Sent if an invitation to a guild fails (the player is busy). 
+	chat(2, "failed to invite",params,"to the guild. reason: busy")
 
 end
--- function EVENT_LOOT_DISTRIBUTION_STARTED(params)
--- 	chat(2, params.itemObject)
--- end
+function EVENT_GUILD_DECLINE_IGNORED(params) --Notification about the impossibility of an invitation to the guild due to ignorance. 
+	chat(2, "failed to invite",params,"to the guild. reason: ignored")
 
+end
+function EVENT_GUILD_MEMBER_ADDED(params) --Sent if a player has been added to the main player's guild. 
+	chat(2, params.name,"was added to the guild. guildmemberid:",params.id)
+	local memberInfo = guild.GetMemberInfo(params.id) --https://alloder.pro/md/LuaApi/FunctionGuildGetMemberInfo.html
+
+	--Automatically changes guild rank depending whether max level or not.
+	if memberInfo.level < 105 then
+		guild.ChangeRank( params.id, 6 ) --https://alloder.pro/md/LuaApi/FunctionGuildChangeRank.html
+	else --9 outlawed, 8 punished, 7 alternate, 6 trial, 5, member, 4 veteran, 3 junior officer, 2 senior officer, 1 unseen.
+		guild.ChangeRank( params.id, 5) --https://alloder.pro/md/LuaApi/FunctionGuildChangeRank.html
+	end
+
+	--Automatically adds comment to the newly joined guildmember.
+	local inviterName = object.GetName(avatar.GetId()) --Gets playername of main player/inviting player.
+	local DescText = "Welcome mail sent to "..memberInfo.name.." lv"..tostring(memberInfo.level).." | invited by "..inviterName.." on "..memberInfo.joinTime.d.."/"..memberInfo.joinTime.m.."/"..memberInfo.joinTime.y
+	guild.SetMemberDescription( params.id, DescText) --Add DescText to the guildmember comment.
+
+	--Automatically gives regular tabard permission to the newly joined guildmember.
+	if guild.CanDistributeTabard( params.id ) then --https://alloder.pro/md/LuaApi/FunctionGuildCanDistributeTabard.html
+		guild.DistributeTabard( params.id, ENUM_TabardType_Common )
+	else
+		chat(2, "There are no regular tabards left to give. Check if inactives can be reclaimed and assign them to the new guild member, please.")
+	end
+
+	--Add the new playername to the guild message
+	guild.GetMessage() --https://alloder.pro/md/LuaApi/FunctionGuildGetMessage.html
+	guild.SetMessage( guild.GetMessage()..params.name..", ") --https://alloder.pro/md/LuaApi/FunctionGuildSetMessage.html
+end
+-- ||Maybe of use for later||
+-- function GetMemberDescrptn() --https://alloder.pro/md/LuaApi/FunctionGuildGetMemberDescription.html
+-- 	local memberDescription = guild.GetMemberDescription( memberId )
+-- 	chat(2, memberDescription)
+-- end
 
 if (avatar.IsExist()) then
 	chat(2,"Loaded.")
