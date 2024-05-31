3ee4d49953715fe3233954f30ce426d751775e74
Dragagon <dragagon.draga@hotmail.com>
2024-05-31 06:24:42 +0200

    Added quite some features to guildBot. Expanded UI with extra options for duration of inactivity of player, as well a button to execute. Added functionality to all of them where I iterate through guild members, get their last logged in time, compare that to the current time in MS, convert that to days and return that to the user. Tabards and Prestige are next, as well as bringing that all together under the button reaction.

--- Settings.lua
+++ Settings.lua
@@ -2,7 +2,7 @@
 guildRecruitmentMessage = "••Avarice•• is welcoming all returning and new players to our guild! What we offer: Experienced in Locus raids, obs raids, nihaz citadel raids, gearing dungeons, % upgrade dungeons, MoS PvE arena, ranked 3x3/6x6 (training) arenas, Dominions & more!" 
 
 --The cooldown between reminders in seconds. Example for 60*30: 60 seconds x 30 = 30 minutes.
-reminderCooldown = 60*30
+reminderCooldown = 60*50
 
 
 
--- SourceCode.lua
+++ SourceCode.lua
@@ -6,37 +6,56 @@
 --Remindes the user to world/zone shout recruitment for the guild at a click of a button.
 -------------------------------------------
 -- WIDGETS
+-- RECRUITMENT BUTTON
 local wtButton = mainForm:GetChildChecked("Button",false)
 wtButton:SetVal("button_label", userMods.ToWString(locales["RecruitmentButtonText"])) --Text can be found in locales.lua
 wtButton:Show(false)
 
+-- GuildBot button to summon and remove the UI.
 local wtGuildBotCallButton = mainForm:GetChildChecked("GuildBotCallButton",false)
 wtGuildBotCallButton:SetVal("button_label", userMods.ToWString("GB"))
 wtGuildBotCallButton:Show(true)
 
+-- The GuildBot UI.
 local wtMainPanel = mainForm:GetChildChecked("MainPanel", false)
-wtMainPanel:Show(false)
-local TextToRemove = wtMainPanel:GetChildChecked("TextToRemove", false)
---TextToRemove:SetVal("value", userMods.ToWString("To remove.."))
+wtMainPanel:Show(true) --CHANGE THIS TO FALSE TO HIDE ON STARTUP.
+local TextToRemove = wtMainPanel:GetChildChecked("TextToRemove", false) --Text indicator asking what to remove; tabards or members.
+--Accessing the text for the header of the addon.
 local wtWindowHeader = wtMainPanel:GetChildChecked("WindowHeader", false)
 local wtHeaderText = wtWindowHeader:GetChildChecked("HeaderText", false)
 wtHeaderText:SetVal("window_header", userMods.ToWString("GuildBot"))
---wtWindowHeader:Show(true)
 
 --local WidgetPanel = wtMainPanel:GetChildChecked("WidgetPanel", false)
 --WidgetPanel:Show(true)
 --local CornerCross = WidgetPanel:GetChildChecked("CornerCross", false)
 
+--guildBot LEFT side.
+--ContentFrame is where the buttons and TextView are inside of.
 local wtContentFrameToRemove = wtMainPanel:GetChildChecked("ContentFrameToRemove", false)
 --wtContentFrameToRemove:Show(false)
+--ItemToRemove is what shows when the using presses the buttons.
 local wtItemToRemove = wtContentFrameToRemove:GetChildChecked("ItemToRemove", false)
-
-
-
+--Buttons
 local wtButtonLeftToRemove = wtContentFrameToRemove:GetChildChecked("ButtonLeftToRemove", false)
 --wtButtonLeftToRemove:Show(false)
 local wtButtonRightToRemove = wtContentFrameToRemove:GetChildChecked("ButtonRightToRemove", false) -- Won't remove
 --wtButtonRightToRemove:Show(false)
+
+--guildBot RIGHT side.
+local wtContentFrameToRemoveByTime = wtMainPanel:GetChildChecked("ContentFrameToRemoveByTime", false)
+local wtInputTextToRemoveByTime = wtContentFrameToRemoveByTime:GetChildChecked("InputTextToRemoveByTime", false)
+--Buttons
+local wtButtonLeftToRemoveByTime = wtContentFrameToRemoveByTime:GetChildChecked("ButtonLeftToRemoveByTime", false)
+local wtButtonRightToRemoveByTime = wtContentFrameToRemoveByTime:GetChildChecked("ButtonRightToRemoveByTime", false)
+--wtButtonRightToRemoveByTime:Show(false)
+
+--GuildBot Removal BUTTON
+local wtButtonRemoveMembersTabards = wtMainPanel:GetChildChecked("ButtonRemoveMembersTabards",false)
+--wtButtonRemoveMembersTabards:SetVal("button_label", userMods.ToWString(locales["RecruitmentButtonText"])) --Used for translating button text in locales.lua.
+--wtButtonRemoveMembersTabards:Show(false)
+
+
+
 
 -------------------------------------------
 local avatarId = nil
@@ -48,11 +67,11 @@ local currentGuildMessage = nil --Placeholder for the current guild's daily mess
 local seconds = 0
 local spamMessageCounter = 2
 local recruitCooldown = 10 --(1.5h 5400)
-local flag = false --Registers to show or hide the GUI GuildBot.
+local GuildBotOpen = false --Registers to show or hide the GUI GuildBot.
 local removalParameters = {} --Table of options for removal of tabards or members.
 removalParameters[0] = "Members"
 removalParameters[1] = "Tabards"
-local i = 0 --Index tracker for changing removalParameters.
+local removalParametersIndex = 0 --Index tracker for changing removalParameters.
 --local currentChatType = nil --No longer used as we had to wait for user to send message before being able to put it back to the original chatCmdSys (chattype)
 Global("guildRecruitmentMessage", nil)
 Global("reminderCooldown", 15) --15 is never used, was necesarry to avoid error. Somehow nil didn't work.
@@ -60,7 +79,7 @@ Global("reminderCooldown", 15) --15 is never used, was necesarry to avoid error.
 function Main()
 	GetAvatarId()
 	--GetEventsId()
-	--ChangeGUI()
+	--ChangeGUI() --test function for widgets.
 	common.RegisterEventHandler(EVENT_GUILD_DECLINE_BUSY, "EVENT_GUILD_DECLINE_BUSY") --https://alloder.pro/md/LuaApi/EventGuildDeclineBusy.html
 	common.RegisterEventHandler(EVENT_GUILD_DECLINE_IGNORED, "EVENT_GUILD_DECLINE_IGNORED") --https://alloder.pro/md/LuaApi/EventGuildDeclineIgnored.html
 	common.RegisterEventHandler(EVENT_GUILD_MEMBER_ADDED, "EVENT_GUILD_MEMBER_ADDED") --https://alloder.pro/md/LuaApi/EventGuildMemberAdded.html
@@ -73,52 +92,120 @@ function Main()
 	--common.RegisterEventHandler(EVENT_NEWS_POST_SELECTED, "EVENT_NEWS_POST_SELECTED") --https://alloder.pro/md/LuaApi/EventNewsPostSelected.html Seems to not work for now. Awaiting feedback from Lafayette
 	RecruitmentReminderTimer()
 	common.RegisterReactionHandler( GuildBotCallButtonClick, "GuildBotCallButtonClick" )
-	DnD.Init(wtButton, nil, true)
+	--Allows the widgets to be DnD
+	DnD.Init(wtButton, nil, true) 
 	DnD.Init(wtGuildBotCallButton, nil, true)
 	DnD.Init(wtMainPanel, nil, true)
 end
--- function EVENT_NEWS_POST_SELECTED(params)
--- 	ruleId = params.ruleId
--- 	local calendarEventId = rules.GetEventByRuleId( ruleId )
--- 	local calendarEventInfo = rules.GetEventInfo( calendarEventId )
--- 	chat(3, "selected post:", calendarEventInfo.name, calendarEventId)
--- end
--- function EVENT_NEWS_POST_LOADED(params)
--- 	chat(3, "hello", params.ruleId)
--- end
-
-function GuildBotCallButtonClick( params )
+function GuildBotCallButtonClick( params ) --Opens and closes the GuildBot UI.
 	if DnD:IsDragging() then return	end
-	if flag == true then
+	if GuildBotOpen == true then
 		wtMainPanel:Show(false)
-		flag = false
+		GuildBotOpen = false
+		--Left GuildBot buttons
 		common.UnRegisterReactionHandler( ButtonLeftToRemoveClick, "ButtonLeftToRemoveClick" )
 		common.UnRegisterReactionHandler( ButtonRightToRemoveClick, "ButtonRightToRemoveClick" )
-	elseif flag == false then
+		--Right GuildBot buttons
+		common.UnRegisterReactionHandler( ButtonLeftToRemoveByTimeClick, "ButtonLeftToRemoveByTimeClick" )
+		common.UnRegisterReactionHandler( ButtonRightToRemoveByTimeClick, "ButtonRightToRemoveByTimeClick" )
+		--RemovalButton
+		common.UnRegisterReactionHandler( ButtonRemoveMembersTabardsClick, "ButtonRemoveMembersTabardsClick" )
+	elseif GuildBotOpen == false then
 		wtMainPanel:Show(true)
-		flag = true
+		GuildBotOpen = true
+		--Left GuildBot buttons
 		common.RegisterReactionHandler( ButtonLeftToRemoveClick, "ButtonLeftToRemoveClick" )
 		common.RegisterReactionHandler( ButtonRightToRemoveClick, "ButtonRightToRemoveClick" )
-		wtItemToRemove:SetVal("value", tostring(removalParameters[i]))
+		--Right GuildBot buttons
+		common.RegisterReactionHandler( ButtonLeftToRemoveByTimeClick, "ButtonLeftToRemoveByTimeClick" )
+		common.RegisterReactionHandler( ButtonRightToRemoveByTimeClick, "ButtonRightToRemoveByTimeClick" )
+		wtItemToRemove:SetVal("value", tostring(removalParameters[removalParametersIndex]))
+		--RemovalButton
+		common.RegisterReactionHandler( ButtonRemoveMembersTabardsClick, "ButtonRemoveMembersTabardsClick" )
 	end
 end
-function ButtonLeftToRemoveClick(params)
-	chat(2,"left click")	
-	i = i - 1
-	if i < 0 then
-		i = GetMaxKey(removalParameters)
-	end
-	wtItemToRemove:SetVal("value", tostring(removalParameters[i]))
+function ButtonRemoveMembersTabardsClick(params)
+	local removalSelected = removalParameters[removalParametersIndex]
+	local inactiveDaysSelected = tonumber(userMods.FromWString(wtInputTextToRemoveByTime:GetText())) --:GetText() returns WString so we unwrap it.
+	--chat(2, removalSelected, inactiveDaysSelected)
+	GuildInfo(inactiveDaysSelected)
 end
-function ButtonRightToRemoveClick(params)
-	chat(2,"right click")
-	i = i + 1
-	if i > GetMaxKey(removalParameters) then
-		i = 0
+function GuildInfo(inactiveDaysSelected)
+	local members = guild.GetMembers()
+	local removalCounter = 0 --Keeps track of how many players will get removed or lose tabard.
+	local removalList = {} --Create a table to display at the end.
+	for i = 0, GetTableSize(members) do --Loop starts
+		local memberInfo = guild.GetMemberInfo( members[ i ] )
+		if memberInfo then
+			local lastOnlineInMs = memberInfo.lastOnlineTime.overallMs
+			local currrentInMs = mission.GetGlobalDateTime().overallMs
+			if CalcDateDiffInDays(currrentInMs, lastOnlineInMs) >= inactiveDaysSelected then
+				removalCounter = removalCounter + 1
+				table.insert(removalList, {key = userMods.FromWString(memberInfo.name), value = CalcDateDiffInDays(currrentInMs, lastOnlineInMs).." day(s)."})
+			end
+		end
+	end --Loop ends
+	chat(2, removalCounter,"player(s) will get removed that are offline for over",inactiveDaysSelected,"days." )
+	chat(2, "Players to be removed are:")
+	for _, pair in ipairs(removalList) do
+		chat(2, pair.key, pair.value)
 	end
-	wtItemToRemove:SetVal("value", tostring(removalParameters[i]))
 end
-function GetMaxKey(table)
+function CalcDateDiffInDays(currrentInMs, lastOnlineInMs)
+	local diffInDays = math.floor((currrentInMs - lastOnlineInMs) / 86400000) --Ms in a day (24h)
+	return diffInDays
+end
+function ButtonLeftToRemoveByTimeClick(params)
+	--local playerInput = wtInputTextToRemoveByTime:GetString() --Works for strings, not numbers --https://alloder.pro/md/LuaApi/FunctionEditLineGetString.html
+	local playerInput = wtInputTextToRemoveByTime:GetText() --https://alloder.pro/md/LuaApi/FunctionEditLineGetString.html
+	if playerInput:IsEmpty() then
+		wtInputTextToRemoveByTime:SetText(userMods.ToWString("0"))
+	else
+		playerInput = userMods.FromWString(playerInput)
+		playerInput = playerInput - 1
+		if playerInput < 0 then
+			playerInput = 0
+		end
+		playerInput = userMods.ToWString(tostring(playerInput))  --To put this back into EditLine widget, needs to be WString. We unwrapped input in order to do +1. Cannot do math on a WString otherwise.
+		wtInputTextToRemoveByTime:SetText(playerInput)
+	end
+	chat(2, "left click", playerInput)
+	--wtInputTextToRemoveByTime:BackspaceCharAtCursorPos() --Backspace function https://alloder.pro/md/LuaApi/FunctionEditLineBackspaceCharAtCursorPos.html
+	--wtInputTextToRemoveByTime:DeleteCharAtCursorPos() --Delete function https://alloder.pro/md/LuaApi/FunctionEditLineDeleteCharAtCursorPos.html
+end
+function ButtonRightToRemoveByTimeClick (params)
+	--local playerInput = wtInputTextToRemoveByTime:GetString() --Works for strings, not numbers --https://alloder.pro/md/LuaApi/FunctionEditLineGetString.html
+	local playerInput = wtInputTextToRemoveByTime:GetText() --https://alloder.pro/md/LuaApi/FunctionEditLineGetString.html
+	if playerInput:IsEmpty() then
+		wtInputTextToRemoveByTime:SetText(userMods.ToWString("0"))
+	else
+		playerInput = userMods.FromWString(playerInput)
+		playerInput = playerInput + 1
+		if playerInput > 999 then
+			playerInput = 999
+		end
+		playerInput = userMods.ToWString(tostring(playerInput))  --To put this back into EditLine widget, needs to be WString. We unwrapped input in order to do +1. Cannot do math on a WString otherwise.
+		wtInputTextToRemoveByTime:SetText(playerInput)
+	end
+	chat(2, "left click", playerInput)
+end
+function ButtonLeftToRemoveClick(params) --Left button on GuildBot UI to rotate through options.
+	removalParametersIndex = removalParametersIndex - 1
+	if removalParametersIndex < 0 then
+		removalParametersIndex = GetMaxKey(removalParameters)
+	end
+	wtItemToRemove:SetVal("value", tostring(removalParameters[removalParametersIndex]))
+	chat(2,removalParameters[removalParametersIndex])
+end
+function ButtonRightToRemoveClick(params) --Right button on GuildBot UI to rotate through options.
+	removalParametersIndex = removalParametersIndex + 1
+	if removalParametersIndex > GetMaxKey(removalParameters) then
+		removalParametersIndex = 0
+	end
+	wtItemToRemove:SetVal("value", tostring(removalParameters[removalParametersIndex]))
+	chat(2,removalParameters[removalParametersIndex])
+end
+function GetMaxKey(table) --Function to figure out the size of table hosting options to rotate through.
     local maxKey = -1
     for k, v in pairs(table) do
         if k > maxKey then
@@ -230,7 +317,7 @@ function EVENT_MISSION_RULE_CHANGED(params) --rule stands for calendar events. I
 end
 function SpamGuildMessage() --Function will run a timer and spam the guild message a few times.
 	--chat(2, "seconds:", seconds) --Logs the time passed.
-	common.LogInfo("common","-"..seconds.."-")
+	--common.LogInfo("common","-"..seconds.."-")
 	seconds = seconds + 1 --Makes time add up.
 	if seconds == 1 or seconds == 20*60 or seconds == 40*60 then --Determines after how many seconds the guild messages gets repeated. *60 for minutes.
 		if spamMessageCounter > 0 then --Checks when to end repeating the guild message.
